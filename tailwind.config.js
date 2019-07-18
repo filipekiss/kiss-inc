2aefb3636bc61efc1b671cfc9242f00a11d06718
Filipe Kiss <hello@filipekiss.com.br>
2019-07-18 15:55:02 +0200

    First Version

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                tolopea: '#2c203b',
+                'pale-magenta': '#ff71e0',
+            },
+            fontSize: {
+                hero: '10rem',
+            },
+            fontFamily: {
+                display: ['Bungee Outline'],
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
