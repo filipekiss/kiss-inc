9d30b6967e0986ae8cf3cfd8bcec468551c2854a
Filipe Kiss <hello@filipekiss.com.br>
2020-02-05 13:58:32 +0100

    New layout, more neon-ish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                tolopea: '#2c203b',
-                'pale-magenta': '#ff71e0',
-            },
-            fontSize: {
-                hero: '10rem',
-            },
-            fontFamily: {
-                display: ['Bungee Outline'],
-            },
-        },
+  theme: {
+    extend: {
+      colors: {
+        tolopea: '#2c203b',
+        'pale-magenta': '#ff71e0',
+        turbo: '#fdd023',
+      },
+      fontSize: {
+        hero: '10rem',
+      },
+      fontFamily: {
+        display: ['Monoton'],
+        neon: ['Cedarville Cursive'],
+      },
     },
-    variants: {},
-    plugins: [],
+  },
+  variants: {},
+  plugins: [],
 };
